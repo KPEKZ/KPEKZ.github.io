8f0b7c1bbca443c28a010b5da05bb71c544eabca
edward <ed_010@mail.ru>
2020-05-07 21:55:32 +0300

    day 2

--- Delivery/js/index.js
+++ Delivery/js/index.js
@@ -1,21 +1,19 @@
+'use strict';
+
 document.addEventListener('DOMContentLoaded', () => {
 
-    const cartButton = document.querySelector('#cart-button');
-    const modal = document.querySelector('.modal');
-    const close = document.querySelector('.close');
+    
 
-    cartButton.addEventListener('click', toggleModal);
+    
 
-    close.addEventListener('click', toggleModal);
-
-    function toggleModal() {
-        modal.classList.toggle("is-open");
-    }
+   
 
     new WOW().init();
 
     //day 1
-
+    const cartButton = document.querySelector('#cart-button');
+    const modal = document.querySelector('.modal');
+    const close = document.querySelector('.close');
     const buttonAuth = document.querySelector(".button-auth");
     const modalAuth = document.querySelector('.modal-auth');
     const closeAuth = document.querySelector('.close-auth');
@@ -24,6 +22,17 @@ document.addEventListener('DOMContentLoaded', () => {
     const userName = document.querySelector('.user-name');
     const buttonOut = document.querySelector('.button-out');
     let login = localStorage.getItem('GloStudy');
+    const cardsRestarants = document.querySelector('.cards-restarants');
+    const containerPromo = document.querySelector('.container-promo');
+    const reustarants = document.querySelector('.reustarants');
+    const menu = document.querySelector('.menu');
+    const logo = document.querySelector('.logo');
+    const cardsMenu = document.querySelector('.cards-menu');
+
+
+    function toggleModal() {
+        modal.classList.toggle("is-open");
+    }
 
     function toogleModalAuth() {
         loginInput.borderColor = '';
@@ -92,4 +101,97 @@ document.addEventListener('DOMContentLoaded', () => {
 
     checkAuth();
 
+
+    function createCardRestarants(){
+        const card =`
+        <a class="card wow fadeInUp" data-wow-delay="0.5s">
+        <img class="card-image" src="./img/image2.jpg" alt="">
+          <div class="card-text">
+            <div class="card-heading">
+                <h3 class="card-title">Тануки</h3>
+                <span class="card-tag tag">60 мин</span>
+            </div>
+            <!-- card-heading -->
+            <div class="card-info">
+                <div class="rating"><img class="rating-star" src="./img/star.svg" alt="star">4.5</div>
+                <div class="price">От 1200 ₽</div>
+                <div class="category">Роллы</div>
+            </div>
+    
+        </div>
+        </a>    
+        `;
+    
+        cardsRestarants.insertAdjacentHTML('beforeend',card);
+    
+    }
+    
+    createCardRestarants();
+    createCardRestarants();
+    createCardRestarants();
+
+
+    function createCardGood(){
+        const card = document.createElement('div')
+        card.className = 'card';
+        card.insertAdjacentHTML('beforeend', `
+        
+        <img src="img/pizza-plus/pizza-classic.jpg" alt="image" class="card-image"/>
+        <div class="card-text">
+            <div class="card-heading">
+                <h3 class="card-title card-title-reg">Пицца Классика</h3>
+            </div>
+            <!-- /.card-heading -->
+            <div class="card-info">
+                <div class="ingredients">Соус томатный, сыр «Моцарелла», сыр «Пармезан», ветчина, салями,
+                    грибы.
+                </div>
+            </div>
+            <!-- /.card-info -->
+            <div class="card-buttons">
+                <button class="button button-primary button-add-cart">
+                    <span class="button-card-text">В корзину</span>
+                    <span class="button-cart-svg"></span>
+                </button>
+                <strong class="card-price-bold">510 ₽</strong>
+            </div>
+        </div>
+        `);
+
+        cardsMenu.insertAdjacentElement('beforeend',card);
+    }
+
+
+
+    function openGoods(event){
+        const target = event.target;
+        const restarant = target.closest('.cards-restarants');
+
+        if (restarant){
+            cardsMenu.textContent = '';
+            containerPromo.classList.add('hide');
+            reustarants.classList.add('hide');
+            menu.classList.remove('hide');
+            createCardGood();
+            createCardGood();
+            createCardGood();
+        }
+
+        
+    }
+
+    
+    cartButton.addEventListener('click', toggleModal);
+
+    close.addEventListener('click', toggleModal);
+
+    cardsRestarants.addEventListener('click', openGoods);
+
+    logo.addEventListener('click', ()=>{
+        containerPromo.classList.remove('hide');
+            reustarants.classList.remove('hide');
+            menu.classList.add('hide');
+    })
 });
+
+
